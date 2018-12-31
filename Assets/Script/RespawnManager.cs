1140406a3c7b12328e2e8755b9da8146557862e4
KHS <kij4300@naver.com>
2018-12-31 22:48:18 +0900

    스폰 구현
    스폰 포인트, 스폰 매니저를 만들었다.

---
+++ Assets/Script/RespawnManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnManager : MonoBehaviour
+{
+    public Transform[] points;
+    public GameObject enemyPrefab;
+
+    public float createTime = 2.0f;
+    public int maxEnemy = 13;
+
+    public bool isGameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = GameObject.Find("SpawnPoint").GetComponentsInChildren<Transform>();
+
+        if(points.Length>0)
+        {
+            StartCoroutine(this.CreateEnemy());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator CreateEnemy()
+    {
+        while(!isGameOver)
+        {
+            int enemyCount = (int)GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+            if(enemyCount<maxEnemy)
+            {
+                yield return new WaitForSeconds(createTime);
+
+                int idx = Random.Range(1, points.Length);
+
+                Instantiate(enemyPrefab, points[idx].position, points[idx].rotation);
+            }
+            else
+            {
+                yield return null;
+            }
+        } 
+    }
+}
