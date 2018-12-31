c77cf9bb5a929f950632ef3cccb662aa36f9b20a
KHS <kij4300@naver.com>
2018-12-31 21:14:00 +0900

    Title Scene
    타이틀 씬을 만들었다.

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TitleScene()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void GameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+        Application.OpenURL("http://google.com");
+#else
+        Application.Quit();
+#endif
+    }
+}
